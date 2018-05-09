0054a146d6c8cbe4d1076f539c8743c9f73e6156
Gus Caplan <me@gus.host>
2018-05-08 23:33:58 -0500

    Tue May 08 2018 23:33:58 GMT-0500 (CDT)

--- src/ivan_tcp.cc
+++ src/ivan_tcp.cc
@@ -6,6 +6,7 @@
 
 using v8::ArrayBuffer;
 using v8::Context;
+using v8::External;
 using v8::Function;
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
@@ -25,21 +26,13 @@ typedef struct {
   uv_buf_t reading;
   Isolate* isolate;
   Persistent<Object> that;
-  Persistent<Function> alloc_connection;
+  Persistent<Function> on_connection;
   Persistent<Function> on_connect;
   Persistent<Function> on_write;
   Persistent<Function> on_read;
   Persistent<Function> on_finish;
   Persistent<Function> on_close;
 } ivan_tcp_t;
-
-#define UNWRAP_OR_RETURN_BUFFER(isolate, buf, val) do {                       \
-  (buf) = reinterpret_cast<ivan_tcp_t*>((val).As<ArrayBuffer>()->GetContents().Data()); \
-  if ((buf) == nullptr) {                                                     \
-    IVAN_THROW_EXCEPTION(isolate, "UNWRAP_OR_RETURN_BUFFER");                 \
-    return;                                                                   \
-  }                                                                           \
-} while (0)
 
 #define HANDLE_UV(isolate, op) do {                                           \
   int ret = (op);                                                             \
@@ -51,63 +44,87 @@ typedef struct {
 
 static void Init(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
-  ivan_tcp_t* that;
-  UNWRAP_OR_RETURN_BUFFER(isolate, that, args[0]);
+  ivan_tcp_t* that = new ivan_tcp_t;
 
   that->isolate = isolate;
-  that->that.Reset(isolate, args[1].As<Object>());
+  that->that.Reset(isolate, args[0].As<Object>());
   that->handle.data = that;
 
   uv_tcp_init(uv_default_loop(), &that->handle);
 
-#define V(i, name) \
-  if (args[i]->IsFunction()) that->name.Reset(isolate, args[i].As<Function>());
-  V(2, alloc_connection);
-  V(3, on_connect);
-  V(4, on_write);
-  V(5, on_read);
-  V(6, on_finish);
-  V(7, on_close);
-#undef V
+  args.GetReturnValue().Set(External::New(isolate, that));
+}
+
+static void on_uv_connection(uv_stream_t* handle, int) {
+  auto that = reinterpret_cast<ivan_tcp_t*>(handle->data);
+  Isolate* isolate = that->isolate;
+  Local<Context> context = isolate->GetCurrentContext();
+
+  ivan_tcp_t* client = new ivan_tcp_t;
+  uv_accept(handle, reinterpret_cast<uv_stream_t*>(&client->handle));
+
+  Local<Function> fn = that->on_connection.Get(isolate);
+  Local<Value> val;
+
+  Local<Value> args[] = { External::New(isolate, client) };
+  USE(fn->Call(context, v8::Undefined(isolate), 1, args));
 }
 
 static void Listen(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
-  ivan_tcp_t* that;
-  UNWRAP_OR_RETURN_BUFFER(isolate, that, args[0]);
+  auto that = reinterpret_cast<ivan_tcp_t*>(args[0].As<External>()->Value());
 
   int port = args[1]->Int32Value();
   String::Utf8Value ip(isolate, args[2]);
+  Local<Function> cb = args[3].As<Function>();
+
+  that->on_connection.Reset(isolate, cb);
 
   struct sockaddr_in addr;
   HANDLE_UV(isolate, uv_ip4_addr(*ip, port, &addr));
 
   HANDLE_UV(isolate, uv_tcp_bind(&that->handle, reinterpret_cast<const struct sockaddr*>(&addr), 0));
 
-  HANDLE_UV(isolate,
-      uv_listen(reinterpret_cast<uv_stream_t*>(&that->handle), 511, [](uv_stream_t* server, int) {
-        auto that = reinterpret_cast<ivan_tcp_t*>(server->data);
-        Isolate* isolate = that->isolate;
-        Local<Context> context = isolate->GetCurrentContext();
+  HANDLE_UV(isolate, uv_listen(reinterpret_cast<uv_stream_t*>(&that->handle), 511, on_uv_connection));
+}
 
-        Local<Function> fn = that->alloc_connection.Get(that->isolate);
-        Local<Value> val;
-        if (fn->Call(context, v8::Undefined(isolate), 0, {}).ToLocal(&val)) {
-          ivan_tcp_t* client;
-          UNWRAP_OR_RETURN_BUFFER(isolate, client, val);
+static void on_uv_connect(uv_connect_t* req, int status) {
+  auto that = reinterpret_cast<ivan_tcp_t*>(req->handle->data);
+  Isolate* isolate = that->isolate;
+  Local<Context> context = isolate->GetCurrentContext();
 
-          uv_accept(server, reinterpret_cast<uv_stream_t*>(&client->handle));
-          Local<Function> cb = that->on_connect.Get(isolate);
-          Local<Value> args[] = { client->that.Get(client->isolate) };
-          USE(cb->Call(context, v8::Undefined(isolate), 1, args));
-        }
-      }));
+  Local<Function> cb = that->on_connect.Get(isolate);
+
+  Local<Value> args[] = { v8::Int32::New(isolate, status) };
+  USE(cb->Call(context, v8::Undefined(isolate), 1, args));
+
+  that->on_connect.Reset();
+}
+
+static void Connect(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  auto that = reinterpret_cast<ivan_tcp_t*>(args[0].As<External>()->Value());
+
+  int port = args[1]->Int32Value();
+  String::Utf8Value ip(isolate, args[2]);
+  Local<Function> cb = args[3].As<Function>();
+
+  that->on_connect.Reset(isolate, cb);
+
+  struct sockaddr_in addr;
+  HANDLE_UV(isolate, uv_ip4_addr(*ip, port, &addr));
+
+  HANDLE_UV(isolate, uv_tcp_connect(
+        &that->connect,
+        &that->handle,
+        reinterpret_cast<const struct sockaddr*>(&addr),
+        on_uv_connect));
 }
 
 void Init(Local<Context> context, Local<Object> target) {
   IVAN_SET_PROPERTY(context, target, "init", Init);
   IVAN_SET_PROPERTY(context, target, "listen", Listen);
-  IVAN_SET_PROPERTY(context, target, "sizeof_ivan_tcp_t", sizeof(ivan_tcp_t));
+  IVAN_SET_PROPERTY(context, target, "connect", Connect);
 }
 
 }  // namespace tcp
